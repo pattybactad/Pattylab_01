9547459390709bd92aa3c40c4a4e20262e1e54b9
pattybactad <bactadpatty@gmail.com>
2023-09-28 10:10:33 -0400

    updated codes inside the js file

--- mainScript.js
+++ mainScript.js
@@ -1,33 +1,61 @@
-//---------------------------THIS IS FOR IMAGE GALLERY CODE---------------------------
+//----------JavaScript Console----------
+console.log("This is Excercise 1: Hello JavaScript!");
 
-let slideIndex = 1;
-showSlides(slideIndex);
+//------------------------FOR String EXAMPLES------------------------
+//declaring & re-declaring var 
+var greet1 = "hi, good day.";
+greet2 = "good morning.";
+console.log(greet2);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+//understanding let
+let greeting = "how are you?";  
+if (false) {//if condition is true, it will both declare the greeting
+    let greeting = "how is it going?";
+    console.log(greeting); // "say Hello instead"
+}//if condition is false, it will only declare the greeting outside the {}
+console.log(greeting); // "say Hi"
+
+
+//------------------------VAR, LET, & CONST------------------------
+var x;
+let y;
+
+// How to use variables:
+x = 10;
+y = 98;
+let z = x + y;
+
+//PI = 10; 
+//const PI = 3.141592653589793;
+const PI = 9;
+
+console.log("PI: " + PI);
+
+//const PI2;
+PI2 = 3.14159265359;
+
+// using camelCase is the most common way of naming variables
+var firstName = "Patty";
+var lastName = "Bactad";
+let fullName = firstName + " " + lastName;
+
+console.log("First Name: " + firstName + " Last Name: " + lastName);
+console.log("Full Name " + fullName);
+console.log("X: " + x + "\n" + "Y: " + y + "\n" + "Z: " + z);
+console.log("this is" + " a " + "combined string with an integer: " + z);
+
+let incrementNum = 0;
+console.log("the number: " + incrementNum);
+incrementNum++;
+console.log("the incremented number: " + incrementNum);
+console.log("This is a test log.");
+
+//----------BUTTON for Java console----------
+function myClickFunction() {
+    console.log("Exercise 2: The button was clicked.");
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+//----------BUTTON as a link redirection----------
+function myClickFunction() {
+    window.open("https://www.linkedin.com/in/patty-mae-bactad/");
 }
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-//---------------------------THIS IS THE ***END*** OF IMAGE GALLERY CODE---------------------------
